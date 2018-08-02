65d9db4de0e31d177795934d6d396de4697c872b
Wen Liang <wen.liang@usfoods.com>
2018-08-02 11:04:59 -0500

    init commit

---
+++ codefest-app/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+
+  private apiUrl = 'https://address-book-demo.herokuapp.com/api/contacts';
+  data: any = {};
+
+  constructor(private http: Http){
+    console.log('hello');
+    this.getContacts();
+    this.getData();
+  }
+
+  getData(){
+    return this.http.get(this.apiUrl)
+    //.map((res: Response) => res.json());
+  }
+
+  getContacts(){
+      this.getData().subscribe(data => {
+        console.log(data);
+        this.data = data;
+      })
+  }
+}
